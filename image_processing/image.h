66551205881c979c0877b7c28c3754650df642c8
Bartłomiej Domański <135234591+kebabee@users.noreply.github.com>
2024-03-31 23:08:37 +0200

    Update image.h

--- image_processing/image.h
+++ image_processing/image.h
@@ -1,3 +1,38 @@
+/*
+Objaśnienie funkcji:
+    1. void save_as(const std::string& filename);
+        Zapisuje obrazek w pliku o podanej nazwie. To jest pierwsza funkcja składowa, którą należy
+    2. void add_watermark();
+        Dodaje do obrazka „znak wodny”, coś prostego, ale widocznego.
+    3. void blurr();
+        Zmiękcza obrazek poprzez zastąpienie wartości składowych kolorów ich średnią z tego piksela i sąsiedztwa Moore'a
+    4. void extract_layer(ColorLayer color_layer);
+        Zamienia obrazek na jedną z trzech warstw odpowiadających palecie RGB. Czyli zeruje dwie składowe koloru, a trzecią pozostawia bez zmian.
+    5. void filter();
+        „Magiczny filtr” polega na zastąpieniu wartości składowych kolorów piksela (x, y) wartościami bezwzględnymi różnic tych składowych w pozycji (x, y) i (x+1, y+1). 
+    6. void flip_horizontally();
+        Odwróć obrazek w poziomie
+    7. void flip_vertically();
+        Odwróć obrazek w pionie
+    8. void inflate();
+        Powiększ obrazek 2 razy. Tam, gdzie w oryginale jest piksel (x, y), tam w powiększeniu jest grupa 4 identycznych pikseli tworzących kwadrat o wierzchołku (2x, 2y).
+    9. void negative();
+        Zastąp obrazek jego negatywem.
+    10. void rotate_clockwise_90();
+        Obróć obrazek o 90 stopni w kierunku obrotu wskazówek zegara.
+    11. void sepia();
+        Zamień obrazek na odcienie szarości, a następnie nadaj mu odcień sepii.
+    12. void shrink();
+        Operacja odwrotna do inflate(), czyli zmniejszamy rozmiar obrazka o czynnik 2. Do piksela (x.y) wpisujemy średnią arytmetyczną, w każdym kanale kolorów liczoną osobno, 
+        z wartości koloru w czterech sąsiednich pikselach oryginału.
+    13. void to_grayscale();
+        Zamień (kolorowy) obrazek na obrazek w kolorach szarości, do każdej składowej średnia arytmetyczna pierwotnych wartości koloru.
+    14. int height() const;
+        Po prostu zwraca wysokość obrazka.
+    15. int width() const;
+        Po prostu zwraca szerokość obrazka.
+*/
+
 #ifndef IMAGE_H
 #define IMAGE_H
 
